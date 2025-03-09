52aa2c6a98ceed1870c3ca61674171c5aebe62a3
Ciro Garcia <ciroivan94@gmail.com>
2025-03-09 17:38:28 -0600

    feat: add support for uuid and script for PipelineForm in src/web/rutes/forms.rs.

--- src/web/rutes/forms.rs
+++ src/web/rutes/forms.rs
@@ -1,15 +1,34 @@
 use serde::{Deserialize, Serialize};
 
+use super::errors::RutesHttpError;
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct PipelineForm {
-    pub name : String,
+    pub name: String,
     pub description: String,
     /* This field is optional for creation */
     pub script: Option<String>,
+    pub uuid: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct NewTask {
-    pub command : String, 
-    pub arguments : String,
+    pub command: String,
+    pub arguments: String,
+}
+
+impl PipelineForm {
+    pub fn get_uuid(&self) -> Result<String, RutesHttpError> {
+        match &self.uuid {
+            Some(s) => Ok(s.to_string()), 
+            _ => Err(RutesHttpError::Default),
+        }
+    }
+
+    pub fn get_script(&self) -> Result<String, RutesHttpError> {
+        match &self.script {
+            Some(s) => Ok(s.to_string()), 
+            _ => Err(RutesHttpError::Default),
+        }
+    }
 }
